f8d9816435cbe5312806595e807f2375db5aece2
Aaron Chou <aaron@ecrowdmedia.com>
2014-04-21 16:46:53 +0800

    Arrange OAuth-api documents by yuidoc

---
+++ docs/api.js
@@ -0,0 +1,16 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Books",
+        "Comments",
+        "Feedback",
+        "Highlights",
+        "Library",
+        "Readings",
+        "me",
+        "users"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
